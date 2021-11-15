4ebf4625d4f7c6e970bdca11f8f5d701646d582a
PabloCas <MadridCastilla@hotmail.com>
2021-11-15 20:12:49 +0100

    Iteration#2

---
+++ Iteration2.js
@@ -0,0 +1,30 @@
+//2. Variables avanzadas
+
+//1.1
+const character = { name: "Jack Sparrow", age: 10 };
+character["age"] = 25;
+
+//1.2
+const firstName = "Jon";
+const lastName = "Snow";
+const age = 24;
+const frase = `Soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos`;
+
+console.log(frase);
+
+//1.3
+const toy1 = { name: "Buss myYear", price: 19 };
+const toy2 = { name: "Rallo mcKing", price: 29 };
+
+console.log(toy1["price"] + toy2["price"]);
+
+
+//1.4
+let globalBasePrice = 10000;
+const car1 = { name: "BMW m&m", basePrice: 50000, finalPrice: 60000 };
+const car2 = { name: "Chevrolet Corbina", basePrice: 70000, finalPrice: 80000 };
+
+globalBasePrice = 25000;
+
+car1["finalPrice"] = 75000;
+car2["finalPrice"] = 95000;
