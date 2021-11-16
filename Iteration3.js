00212ea9dd5e1d83e5e2fd29d5ddbfd551cb5647
PabloCas <MadridCastilla@hotmail.com>
2021-11-16 07:05:42 +0100

    Iteration#3

---
+++ Iteration3.js
@@ -0,0 +1,31 @@
+//3. Operadores
+
+//1.1
+let num1 = 10;
+let num2 = 5;
+let resultado = (num1 * num2);
+alert(resultado);
+
+//1.2
+let num3 = 10;
+let num4 = 2;
+let resultado2 = (num3 / num4);
+alert(resultado2);
+
+1.3;
+let num5 = 15;
+let num6 = 9;
+let resto = (num5 % num6);
+alert(resto);
+
+//1.4
+y = 10;
+z = 5;
+x = (z + y);
+console.log(x);
+
+//1.5
+y = 10;
+z = 5;
+x = (y * z);
+console.log(x);
